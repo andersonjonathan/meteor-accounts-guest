8eb0a19691dd0ca15e6f236cb8f5766555a80db0
jonathan <jonan099@student.liu.se>
2016-12-30 12:34:17 +0100

    Solved versions issues

--- package.js
+++ package.js
@@ -1,20 +1,20 @@
 Package.describe({
     summary: "Automatically add visitor as anonymous guest with userId",
-    version: "0.1.13",
-    name: "artwells:accounts-guest",
-    git: "https://github.com/artwells/meteor-accounts-guest.git"
+    version: "1.0.0",
+    name: "andersonjonathan:accounts-guest",
+    git: "https://github.com/andersonjonathan/meteor-accounts-guest.git"
 });
 
 Package.onUse(function (api) {
     api.versionsFrom("METEOR@0.9.0");
-    api.use(['accounts-base','deps', 'blaze@2.0.4'], 'client');
-    api.use(['accounts-base', 'mongo@1.1.0', 'check', 'random'], 'server');
+    api.use(['accounts-base','deps', 'blaze@2.2.0'], 'client');
+    api.use(['accounts-base', 'mongo@1.1.14', 'check', 'random'], 'server');
     api.use('accounts-password', 'server', { weak: true });
     api.use('underscore', 'server');
-    api.use('brettle:accounts-patch-ui@0.0.1');
-    api.use('brettle:accounts-login-state@0.0.1');
-    api.use('brettle:accounts-add-service@0.3.0', ['client', 'server'], { weak: true });
-    api.use('brettle:accounts-multiple@0.3.0', ['client', 'server'], { weak: true });
+    api.use('brettle:accounts-patch-ui@0.1.3');
+    api.use('brettle:accounts-login-state@0.0.4');
+    api.use('brettle:accounts-add-service', ['client', 'server'], { weak: true });
+    api.use('brettle:accounts-multiple', ['client', 'server'], { weak: true });
     api.add_files('accounts-guest.js', ['client','server']);
     api.export('AccountsGuest');
     api.add_files('accounts-guest-server.js', 'server');
@@ -25,7 +25,7 @@ Package.onUse(function (api) {
 Package.onTest(function (api) {
     api.versionsFrom("METEOR@0.9.0");
     api.use(['accounts-base', 'accounts-password', 'mongo', 'tinytest','deps','ddp'], ['client','server']);
-    api.use('artwells:accounts-guest');
+    api.use('andersonjonathan:accounts-guest');
     api.add_files('accounts-guest-server-tests.js', 'server');
     api.add_files('accounts-guest-client-tests.js', 'client');
 });
@@ -33,3 +33,4 @@ Package.onTest(function (api) {
 Npm.depends({
     'moniker': '0.1.2'
 });
+
